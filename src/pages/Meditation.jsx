e419d08f8e203d5b5dd4627a4a9995f125317041
Kristina Voit <k.voit.v.thun@web.de>
2022-01-30 09:14:24 +0100

    more bug-fixing

--- src/pages/Meditation.jsx
+++ src/pages/Meditation.jsx
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-import Header from "../components/Header/Header.jsx";
 import Breathe from "../Meditation/Breathe";
 
 const Home = () => {
@@ -12,20 +10,3 @@ const Home = () => {
   );
 };
 export default Home;
-
-const Greeting = styled.div`
-  font-size: 20px;
-`;
-
-const ButtonStyle = styled.a`
-  background: #f5f9f9;
-  color: #509b9b;
-  border: none;
-  font-weight: bold;
-  text-decoration: none;
-  border-radius: 15px;
-  margin-left: 5px;
-  margin-top: 1rem;
-  cursor: pointer;
-  font-family: "Montserrat", sans-serif;
-`;

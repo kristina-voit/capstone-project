19446fbe7512ed112e6387bb46be2230f7c22824
Kristina Voit <k.voit.v.thun@web.de>
2022-02-02 11:23:44 +0100

    Changed counting for breathing exercise

--- src/components/Meditation/Breathe.jsx
+++ src/components/Meditation/Breathe.jsx
@@ -4,7 +4,7 @@ import "../Meditation/styles.css";
 function Breathe() {
   const [text, setText] = useState("Ausatmen");
   const [grow, setGrow] = useState(false);
-  const [shrink, setShrink] = useState(false);
+  const [shrink, setShrink] = useState(true);
   const [count, setCount] = useState(0);
 
   const breathIn = () => {
@@ -22,29 +22,27 @@ function Breathe() {
   };
 
   return (
-    <main>
-      <div id="Universal">
-        <div
-          className={`moon-container ${grow ? "grow" : ""} ${
-            shrink ? "shrink" : ""
-          }`}
-          id="animation"
-          onAnimationIteration={count === 1 ? breathOut : breathIn}
-        >
-          <div className="text-area">
-            <h2>
-              {count} : {text}
-              <p>Relax and unwind</p>
-            </h2>
-          </div>
-          <div className="moon"></div>
+    <div id="Universal">
+      <div
+        className={`moon-container ${grow ? "grow" : ""} ${
+          shrink ? "shrink" : ""
+        }`}
+        id="animation"
+        onAnimationIteration={count === 1 ? breathOut : breathIn}
+      >
+        <div className="text-area">
+          <h2>
+            {count} : {text}
+            <p>Relax and unwind</p>
+          </h2>
+        </div>
+        <div className="moon"></div>
 
-          <div className="yogi-container">
-            <span className="yogi"></span>
-          </div>
+        <div className="yogi-container">
+          <span className="yogi"></span>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
 
